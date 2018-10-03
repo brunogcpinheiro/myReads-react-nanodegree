e6a686a557d460363c49b21b0505fbeb17b02610
Bruno Pinheiro <brunogcpinheiro@hotmail.com>
2018-10-03 10:07:59 -0300

    add global styles w/ styled components and add booksAPI

---
+++ src/styles/global.js
@@ -0,0 +1,14 @@
+import { injectGlobal } from "styled-components";
+
+injectGlobal`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Varela Round', sans-serif;
+  }
+
+  body {
+    background-color: #2f3640;
+  }
+`;
